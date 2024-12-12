3b4689a71299bcde333e3aa5a503962d4cb90ec6
tonnytech <tonnytei4@gmail.com>
2024-12-12 09:41:26 +0300

    project and tailwind setup

---
+++ src/pages/HomePage.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const HomePage = () => {
+  return (
+    <div className='min-h-screen bg-blue-100 flex items-center justify-center'>
+      <h1 className='text-4xl font-bold text-blue-500'>Hello, Tailwind!</h1>
+    </div>
+  );
+}
+
+export default HomePage
